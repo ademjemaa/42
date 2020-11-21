3e778f758379fdb3a5471554647f8afefbbc8deb
adjemaa <ademlolplayer@gmail.com>
2020-11-21 20:39:58 +0100

    why not allow sem_init :(

---
+++ philosophers/philo_two/function.c
@@ -0,0 +1,47 @@
+#include "header.h"
+
+unsigned long   the_time(void)
+{
+    struct timeval eat;
+
+    gettimeofday(&eat, NULL);
+    if (time_now == 0)
+        time_now = (eat.tv_sec * 1000) + (eat.tv_usec / 1000);
+    return ((eat.tv_sec * 1000) + (eat.tv_usec / 1000));
+}
+
+void    pick_up_forks(int cur)
+{
+    sem_wait(stru.two_forks);
+    sem_wait(stru.forks);
+    ft_locked_print("has taken a fork\n", cur);
+    sem_wait(stru.forks);
+    ft_locked_print("has taken a fork\n", cur);
+    sem_post(stru.two_forks);
+}
+
+void    drop_forks(void)
+{
+    sem_post(stru.forks);
+    sem_post(stru.forks);
+}
+
+void    *function(void  *args)
+{
+    int cur = *(int*)args;
+
+    if (!check_stats())
+        return NULL;
+    pick_up_forks(cur);
+    ft_locked_print("is eating\n", cur);
+    stru.philos[cur].last_meal = the_time();
+    usleep(stru.time_eat * 1000);
+    drop_forks();
+    stru.philos[cur].total_meals++;
+    ft_locked_print("is sleeping\n", cur);
+    usleep((stru.time_sleep - stru.time_eat) * 1000);
+    ft_locked_print("is thinking\n", cur);
+    if (stru.state == 0)
+        function(args);
+    return(0);
+}
---
+++ philosophers/philo_two/philo_uno.c
@@ -0,0 +1,85 @@
+#include "header.h"
+
+int     check_stats(void)
+{
+    int index = 0;
+    for (int i = 0; i < stru.philo; i++)
+    {
+        if (stru.philos[i].total_meals == stru.total_must_eat)
+            index++;
+        else if ((unsigned long)(the_time() - stru.philos[i].last_meal) >= (unsigned long)stru.time_die)
+        {
+            ft_locked_print("has died\n", i);
+            stru.state = 1;
+            return(0);
+        }
+    }
+    if (index == stru.philo && stru.total_must_eat > 0)
+    {
+        stru.state = 1;
+        return(0);
+    }
+    return (1);
+}
+
+int    init_vars(char **argv, int argc)
+{
+    unsigned long current;
+    if (!(argc == 5 || argc == 6))
+        return (0);
+    for (int i = 1; i < argc; i++)
+        for (int j = 0; argv[i][j]; j++)
+            if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+                return (0);
+    stru.philo = ft_atoi(argv[1]);
+    stru.time_die = ft_atoi(argv[2]);
+    stru.time_eat = ft_atoi(argv[3]);
+    stru.time_sleep = ft_atoi(argv[4]);
+    stru.state = 0;
+    stru.total_must_eat = -1;
+    if (argc == 6)
+    {
+        stru.total_must_eat = ft_atoi(argv[5]);
+        if (stru.total_must_eat < 1)
+            return (0);
+    }
+    if ((stru.philos = malloc(sizeof(t_phil) * stru.philo)) == NULL)
+		return (0);
+    current = the_time();
+    stru.forks = sem_open("forks", O_CREAT, S_IRWXU, stru.philo);
+    stru.print = sem_open("print", O_CREAT, S_IRWXU, 1);
+    stru.two_forks = sem_open("two forks", O_CREAT, S_IRWXU, 1);
+    for (int i = 0; i < stru.philo; i++)
+    {
+        stru.philos[i].id = i;
+        stru.philos[i].last_meal = current;
+        stru.philos[i].total_meals = 0;
+    }
+    return (1);
+}
+
+int main(int argc, char **argv)
+{
+    sem_unlink("forks");
+    sem_unlink("two forks");
+    sem_unlink("print");
+    time_now = 0;
+    if (init_vars(argv, argc) == 0)
+    {
+        ft_putstr("config error\n");
+        if (stru.philos != NULL)
+            free(stru.philos);
+        return (0);
+    }
+    for (int i = 0; i < stru.philo; i++)
+        pthread_create(&stru.philos[i].thread_id, NULL, 
+                       function, (void*)&stru.philos[i].id);
+    while (check_stats());
+    sem_unlink("forks");
+    sem_unlink("two forks");
+    sem_unlink("print");
+    sem_close(stru.forks);
+    sem_close(stru.two_forks);
+    sem_close(stru.print);
+    free(stru.philos);
+}
